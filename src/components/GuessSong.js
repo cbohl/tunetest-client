a75c5b2d72c6f6dd18e21038ed3722c93a2aec6c
Christopher Bohl <chrisrbohl@gmail.com>
2022-11-30 17:31:25 -0600

    Creates empty GuessSong component and uses it in index.

---
+++ src/components/GuessSong.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GuessSong = (props) => {
+
+    return(<h2>In the guessSong component</h2>)
+    // if state.guess == props.songTitle
+}
+
+export default GuessSong
