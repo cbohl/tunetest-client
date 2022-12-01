9bc4bf8aaa8b30e6e338cccac791001f930a4465
Christopher Bohl <chrisrbohl@gmail.com>
2022-12-01 11:54:06 -0600

    Handles song guess.

--- src/components/GuessSong.js
+++ src/components/GuessSong.js
@@ -3,18 +3,41 @@ import React, {useState} from 'react'
 const GuessSong = (props) => {
 
     const [guess, setGuess] = useState("")
-    
+    const songTitle = "Yesterday"
+
+    const handleGuess = () => {
+        console.log("Guess attempt")
+        if(guess === songTitle){
+            alert('Correct!')
+        }else{
+            console.log('Incorrect guess')
+        }
+    } 
+
     return(
         <div>
             <h2>In the guessSong component</h2>
-            <input
-                id="song-guess"
-                className="form-field"
-                type="text"
-                name="Song Guess"
-                value={guess}
-                onChange={e => setGuess(e.target.value)}
-            />
+            <div>
+                <input
+                    id="song-guess"
+                    className="form-field"
+                    type="text"
+                    name="Song Guess"
+                    value={guess}
+                    onChange={e => setGuess(e.target.value)}
+                />
+            </div>
+            <div>
+                <button
+                    id="song-guess-submit"
+                    className="submit-button"
+                    type="submit"
+                    value="Hello!"
+                    onClick={e => handleGuess() }
+                >
+                    Guess   
+                </button>   
+            </div>     
         </div>
     )
 }
