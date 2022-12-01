e47fd9470260e3c802f9fdcd2e50b258ab70ebb1
Christopher Bohl <chrisrbohl@gmail.com>
2022-11-30 23:41:32 -0600

    Makes song guess input functional.

--- src/components/GuessSong.js
+++ src/components/GuessSong.js
@@ -2,20 +2,23 @@ import React, {useState} from 'react'
 
 const GuessSong = (props) => {
 
-    const [guess, setGuess] = useState("test")
-
+    const [guess, setGuess] = useState("")
+    
     return(
         <div>
             <h2>In the guessSong component</h2>
             <input
                 id="song-guess"
-                class="form-field"
+                className="form-field"
                 type="text"
                 name="Song Guess"
                 value={guess}
+                onChange={e => setGuess(e.target.value)}
             />
         </div>
     )
 }
+
+// onChange={(e) => handleGuessChange(e.currentTarget.value)}
 
 export default GuessSong
