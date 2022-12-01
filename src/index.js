47ae0ccac65e2025a31014aa2dcb91d85dc31466
Christopher Bohl <chrisrbohl@gmail.com>
2022-12-01 13:37:11 -0600

    Lints index.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import GuessSong from './components/GuessSong';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GuessSong from "./components/GuessSong";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <h1>Test!</h1>
-    <GuessSong/>
-  </React.StrictMode>
+    <React.StrictMode>
+        <h1>Test!</h1>
+        <GuessSong/>
+    </React.StrictMode>
 );
