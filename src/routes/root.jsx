bd1f07e0d2c65344138cebd1755159e7009bad31
Christopher Bohl <chrisrbohl@gmail.com>
2023-01-09 16:55:18 -0600

    Makes index list of games render dynamically from a GraphQL queery.

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,26 +1,37 @@
 /* eslint-disable */
 import { Link } from "react-router-dom";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { useQuery, gql } from "@apollo/client";
+
+
+
+const GET_ARTISTS = gql`
+query allArtists {
+    allArtists {
+        lastName
+    }
+}`;
+
 
 
 export default function Root() {
+    const { data, loading, error } = useQuery(GET_ARTISTS);
+    if(loading == true){return(<h1>Loading!</h1>)}
+    // else debugger
     return (
       <div className="flex min-h-screen justify-center">
-        <div className="grid max-w-3xl min-w-[60%] max-h-screen grid-rows-4 text-left">
+        <div className="grid max-w-3xl min-w-[60%] max-h-screen grid-rows-4 text-center">
           <h1 className="rows-span-1">Welcome to the Music Guessing App!</h1>
-          <nav className="rows-span-3">
+          <nav>
             <ul className="grid grid-rows-3 max-h-screen mx-auto">
-              <li className="rows-span-1">
-                <Link to={`games/1`} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Play The Beatles Game</Link>
-                <img src= {"http://localhost:5000/images/" + "Beatles" + "2.webp"} className="h-full"></img>
-              </li>
-              <li className="rows-span-1">
-                <Link to={'games/2'} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Play The Backstreet Boys Game</Link>
-                <img src= {"http://localhost:5000/images/" + "Backstreet Boys" + "2.webp"} className="h-full"></img>
-              </li>
-              <li className="rows-span-1 object-contain">
-                <Link to={'games/3'} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Play The Easy Songs Game</Link>
-                <img src= {"http://localhost:5000/images/" + "Songs" + "2.webp"} className="h-full"></img>
-              </li>
+              {data.allArtists.map((artist, i) => {
+                return(
+                  <li className="rows-span-1" key={i}>
+                    <Link to={ "games/" + (i + 1) } className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Play The {artist.lastName} Game</Link>
+                    <img src= {"http://localhost:5000/images/" + artist.lastName + "2.webp"} className="mx-auto h-full"></img>
+                  </li>
+                )
+              })}
             </ul>
           </nav>
         </div>
