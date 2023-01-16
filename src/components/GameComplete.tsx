b76b783b96fe6788420af9c58e4953bb90d9d3e5
Christopher Bohl <chrisrbohl@gmail.com>
2023-01-16 15:24:22 -0600

    Fixes formatting of Game Complete.

--- src/components/GameComplete.tsx
+++ src/components/GameComplete.tsx
@@ -149,32 +149,23 @@ const GameComplete = ({artist, songsList, gameOver}: props) => {
         return(
             <>
                 <div className="flex min-h-screen justify-center">
-                    <div className="grid max-w-3xl min-w-[60%] max-h-72 grid-rows-3 text-center">
-                        <div>
-                            <h3> it goes here</h3>
-                            <h3>{scoreRecords[0].score}</h3>
-                        </div>
-                        <div>
-
-                        </div>
-                        <div>
-
-                        </div>
-                        <div>
-
-                        </div>                 
-                        <div>
-                            <div className="mt-5">
-                                <h4>Guesses Correct</h4>
-                                <h1>{totalGuessesCorrect()}</h1>
+                    <div className="grid max-w-3xl min-w-[60%] max-h-screen grid-rows-3 text-center">
+                        <div className="rows-span-1">
+                            <div>
+                                <div className="mt-5">
+                                    <h4>Guesses Correct</h4>
+                                    <h1>{totalGuessesCorrect()}</h1>
+                                </div>
+                            </div>
+                            <div>
+                                <h1>Thanks for playing!</h1>
+                            </div>
+                            <div className="my-5">
+                                <a href={'/'} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Go back to home page</a>
                             </div>
                         </div>
-                        <div>
-                            <h1>Thanks for playing!</h1>
-                            <a href={'/'}>Go back to main page</a>
-                        </div>
                         { !scoreSubmitted ?
-                            <div>
+                            <div className="rows-span-1">
                                 <h1>Submit your score!</h1>
                                 <div>
                                     <input
@@ -205,13 +196,11 @@ const GameComplete = ({artist, songsList, gameOver}: props) => {
                                 </div>
                             </div>
                             :
-                            <div className="mx-auto block h-64 w-30">
-                                <img src= {process.env.REACT_APP_API_URL + "/static/images/" + artist.lastName + "2.webp"} className="object-cover scale-100 hover:scale-125 ease-in duration-500 z-10"></img>
+                            <div className="rows-span-1"> 
+                                <div className="mx-auto block lg:h-5/6 lg:w-1/3 sm:h-5/6 sm:w-1/4 h-2/3 w-1/2 rounded-md overflow-hidden group">
+                                    <img src= {process.env.REACT_APP_API_URL + "/static/images/" + artist.lastName + "2.webp"} className="object-cover scale-100 z-10"></img>
+                                </div>
                             </div>
-
-                            
-
-
                         }
                         <div>
                             {  
