d5fd56c4acc8ee693a0bd70bbda959b898b31bcd
Christopher Bohl <chrisrbohl@gmail.com>
2023-01-19 17:25:41 -0600

    Refactors Root component.

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -3,8 +3,6 @@ import { Link } from "react-router-dom";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import { useQuery, gql } from "@apollo/client";
 
-
-
 const GET_ARTISTS = gql`
 query allArtists {
     allArtists {
@@ -12,12 +10,11 @@ query allArtists {
     }
 }`;
 
-
-
 export default function Root() {
     const { data, loading, error } = useQuery(GET_ARTISTS);
+
     if(loading == true){return(<h1>Loading!</h1>)}
-    // else debugger
+
     return (
       <div className="flex min-h-screen justify-center">
         <div className="grid max-w-3xl min-w-[60%] max-h-screen grid-rows-4 text-center">
@@ -28,23 +25,15 @@ export default function Root() {
                 return(
                   <li className="rows-span-1" key={i}>
                       <div className="mx-auto block lg:h-5/6 lg:w-1/3 sm:h-5/6 sm:w-1/4 h-2/3 w-1/2 rounded-md overflow-hidden group">
-                        {/* <div> */}
                           <div className="absolute my-auto justify-center items-center left-1/2 transform -translate-x-1/2 z-30 hidden group-hover:block">
                             <Link to={ "games/" + (i + 1) } className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Play The {artist.lastName} Game</Link>
-                            {/* <h1 className="absolute text-green-800 font-bold">Play The {artist.lastName} Game</h1> */}
                           </div>
-                        {/* </div> */}
                         <img src= {process.env.REACT_APP_API_URL + "/static/images/" + artist.lastName + "2.webp"} className="object-cover scale-100 hover:scale-125 ease-in duration-500 z-10"></img>
                       </div>
                   </li>
                 )
               })}
             </ul>
-                      {/* <div className="group">
-                        <h1 className="absolute text-green-800 font-bold">TEST TEST TEST</h1>
-                        <img class="invisible group-hover:visible" src= {process.env.REACT_APP_API_URL + "/static/images/" + "Beatles" + "2.webp"} className="scale-100 hover:scale-125 ease-in duration-500 z-10"></img>
-                        <p className="invisible group-hover:visible">I am hidden until my parent is hovered!</p>
-                      </div> */}
           </nav>
         </div>
       </div>
