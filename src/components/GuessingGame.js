c26e8e38195d3583caff582f64228496a5bd6677
Christopher Bohl <chrisrbohl@gmail.com>
2023-01-19 18:19:19 -0600

    Refactors GuessingGame component.

--- src/components/GuessingGame.js
+++ src/components/GuessingGame.js
@@ -10,28 +10,16 @@ import GameComplete from "./GameComplete.tsx";
 import styles from "./GuessingGame.module.css";
 import "html-midi-player";
 import { useParams } from 'react-router-dom'
-
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import { useQuery, gql } from "@apollo/client";
 
-
-const GET_SONGS = gql`
-    query GetSongs {
-        songs {
-            id
-            title
-            midiFileURL
-        }
-    }`;
-
-const GET_USERS = gql`
-    query allUsers {
-        allUsers{
-            email 
-            name
-        }
-    }
-    `;
+// const GET_SONGS = gql`
+//     query GetSongs {
+//         songs {
+//             id
+//             title
+//             midiFileURL
+//         }
+//     }`;
 
 const GET_ARTISTS = gql`
     query allArtists {
@@ -46,7 +34,7 @@ const GET_ARTISTS = gql`
         }
     }`;
 
-    const GuessingGame = (props) => {
+const GuessingGame = (props) => {
     let [songIndex, setSongIndex] = useState(0);
     let [gameStart, setGameStart] = useState(false);
     let [gameOver, setGameOver] = useState(false);
@@ -54,26 +42,15 @@ const GET_ARTISTS = gql`
     let [artist, setArtist] = useState()
     let { gameId } = useParams();
 
-    console.log("This is the game id!!!", gameId)
-
-    // const { data, loading, error } = useQuery(GET_SONGS);
     const { data, loading, error } = useQuery(GET_ARTISTS);
-
 
     useEffect(() => {
         if(loading === false && data){
-            console.log("setting ssongs!");
-            console.log("here is the data", data)
-
-            // debugger;
-
             let blankSong = {}
             let artist1 = data.allArtists[gameId - 1]
             let blankArtist = {}
             let songs3 = data.allArtists[gameId - 1].songs
-            console.log("here is songs3", songs3)
             let songs4 = []
-            // let songs5 = null
 
             songs3.map(({id, title, midiFilePath}, i) => {
                 blankSong.id = id
@@ -81,49 +58,13 @@ const GET_ARTISTS = gql`
                 blankSong.isCorrectlyGuessed = false
                 blankSong.isCurrent = false
                 blankSong.midiFilePath = midiFilePath
-                console.log("1", songs4)
-                console.log("2", blankSong)
                 songs4.push(blankSong)
-                console.log("3", songs4)
-                // debugger;
                 blankSong = {}
-                // songs4[i].id = id
-                // songs4[i].title = title
-            })
-
-            // debugger;
-
-            // artist1.map(({id, firstName, lastName}, i) => {
-            //     blankArtist.id = id
-            //     blankArtist.firstName = firstName
-            //     blankArtist.lastName = lastName
-            // })
-
-
-            // songs5 = songs4
-
-
-            // console.log("songs4", songs4)
-            // songs3.forEach(
-            //     function(song){
-            //         song.isCorrectlyGuessed = false;
-            //         song.isCurrent = false;
-            //     }
-            // );
-            
+            })            
             setSongs2(songs4)
             setArtist(artist1)
         }
-        console.log("in use effect")
     }, [data]);
-
-
-
-            // songs3[0].isCurrent = true;
-
-        // console.log("HERE IT IS", SONGS);
-
-
 
     const toastCorrectGuess = () => toast('Good guess!');
     const toastIncorrectGuess = () => toast("Incorrect guess!");
@@ -144,8 +85,6 @@ const GET_ARTISTS = gql`
     };
 
     const skipSong = () => {
-        console.log(data)
-        // debugger;
         if(songIndex < songs2.length -1){
             clickPlay();
             setSongIndex(songIndex + 1);
@@ -154,32 +93,8 @@ const GET_ARTISTS = gql`
         };
         
         return(
-        
             <div>
-                {/* <div> */}
-                    {/* <p>test</p> */}
-                    {/* <p> */}
-                        {/* { songs2.map(({id, title}, i) => { */}
-                            {/* // return ( */}
-                                {/* // <div key={i}> */}
-                                    {/* <h1> hi there!</h1> */}
-                                    {/* <h2> {id}</h2> */}
-                                    {/* <h2> {title} </h2> */}
-                                {/* // </div> */}
-                            {/* ) */}
-                        {/* // })} */}
-                    {/* </p> */}
-                    {/* <h2> {songs2.length}</h2> */}
-                {/* </div> */}
-
                 <div>
-                    {/* {songs2 != [] ?
-                        <p>{songs2[1].title}</p>
-                    :
-                        <p> nothing yet</p>
-                    } */}
-                    
-                    {/* <p>{songs2?[1].name}</p> */}
                 </div>
 
                 <div>
@@ -203,11 +118,13 @@ const GET_ARTISTS = gql`
                                                     "NextMidiPlayer": songIndex == i-1, 
                                                     "HiddenMidiPlayer": songIndex != i
                                                 }) } key = {i}>
-                                                    <midi-player
-                                                        src= {process.env.REACT_APP_API_URL + "/static/" + s.midiFilePath}
-                                                        loop
-                                                    >
-                                                    </midi-player>
+                                                    {/* <audio> */}
+                                                        <midi-player
+                                                            src= {process.env.REACT_APP_API_URL + "/static/" + s.midiFilePath}
+                                                            loop
+                                                        >
+                                                        </midi-player>
+                                                    {/* </audio> */}
                                                 </div>
                                             );
                                         })}
@@ -220,13 +137,6 @@ const GET_ARTISTS = gql`
                                     </div>
                                 </div>
                             </div>
-                            {/* <div id="super">
-                                <h1 className="testclass" id="special"> Current song {songs2[songIndex].title}</h1>
-                                <h2> Current link {songs2[songIndex].midiLink}</h2>
-                                <h3> Current index {songIndex}</h3>
-                                <h2> Test </h2>
-                                <h2> Test2 </h2>
-                            </div> */}
                         </div>
                         
                         <GameComplete artist={artist} songsList={songs2} gameOver={gameOver}></GameComplete>
