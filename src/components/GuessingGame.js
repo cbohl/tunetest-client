452626212461f0b6861c7a0c169cac6e74c6da54
Christopher Bohl <chrisrbohl@gmail.com>
2023-01-24 15:24:55 -0600

    Improves GuessingGame component by only making GraphQL request data specific to the artist.

--- src/components/GuessingGame.js
+++ src/components/GuessingGame.js
@@ -21,10 +21,22 @@ import { useQuery, gql } from "@apollo/client";
 //         }
 //     }`;
 
-const GET_ARTISTS = gql`
-    query allArtists {
-        allArtists {
-            id
+// const GET_ARTISTS = gql`
+//     query allArtists {
+//         allArtists {
+//             id
+//             firstName
+//             lastName
+//             songs {
+//                 title
+//                 midiFilePath
+//             }
+//         }
+//     }`;
+
+const GET_ARTIST_INFO = gql`
+    query getArtistInfo($id: Int) {
+        getArtistInfo(id: $id) {
             firstName
             lastName
             songs {
@@ -32,37 +44,61 @@ const GET_ARTISTS = gql`
                 midiFilePath
             }
         }
-    }`;
+    }
+`;
+
+
+//     const GET_ARTIST_SCORE_RECORDS = gql`
+//     query GetArtistScoreRecords($artistId: Int) {
+//         getArtistScoreRecords(artistId: $artistId) {
+//             username
+//             score
+//         }
+//     }
+// `
 
 const GuessingGame = (props) => {
     let [songIndex, setSongIndex] = useState(0);
     let [gameStart, setGameStart] = useState(false);
     let [gameOver, setGameOver] = useState(false);
-    let [songs2, setSongs2] = useState([])
+    let [songs, setSongs] = useState([])
     let [artist, setArtist] = useState()
-    let { gameId } = useParams();
-
-    const { data, loading, error } = useQuery(GET_ARTISTS);
-
+    let  { gameId }  = useParams();
+    
+    // debugger;
+    
+    // const { data, loading, error } = useQuery(GET_ARTISTS);
+    // const { data, loading, error } = useQuery(GET_ARTIST_INFO, {variables: {id: useParams()}});
+    
+    // const { data: queryData, loading: queryLoading, error: queryError } = useQuery(GET_ARTIST_SCORE_RECORDS, {variables: {artistId: artist.id}});
+    console.log("this is the gameId", gameId)
+    // console.log("this is hte params", useParams())
+    // debugger;
+    const { data, loading, error } = useQuery(GET_ARTIST_INFO, {variables: {id: parseInt(gameId)}});
+    
     useEffect(() => {
         if(loading === false && data){
+            // debugger;
             let blankSong = {}
-            let artist1 = data.allArtists[gameId - 1]
-            let blankArtist = {}
-            let songs3 = data.allArtists[gameId - 1].songs
-            let songs4 = []
+            // let songs3 = data.allArtists[gameId - 1].songs
+            let updatedSongs = []
 
-            songs3.map(({id, title, midiFilePath}, i) => {
+            // let newSongs = data.getArtistInfo.songs
+            // newSongs[0].isCorrectlyGuessed = false
+            // debugger;
+
+            data.getArtistInfo.songs.map(({id, title, midiFilePath}, i) => {
                 blankSong.id = id
                 blankSong.title = title
                 blankSong.isCorrectlyGuessed = false
                 blankSong.isCurrent = false
                 blankSong.midiFilePath = midiFilePath
-                songs4.push(blankSong)
+                updatedSongs.push(blankSong)
                 blankSong = {}
-            })            
-            setSongs2(songs4)
-            setArtist(artist1)
+            })
+
+            setSongs(updatedSongs)
+            setArtist(data.getArtistInfo)
         }
     }, [data]);
 
@@ -74,9 +110,9 @@ const GuessingGame = (props) => {
     };
 
     const nextSong = () => {
-        songs2[songIndex].isCorrect = true;
+        songs[songIndex].isCorrect = true;
         
-        if(songIndex < songs2.length -1){
+        if(songIndex < songs.length -1){
             clickPlay();
             setSongIndex(songIndex + 1);
         }else{
@@ -85,7 +121,7 @@ const GuessingGame = (props) => {
     };
 
     const skipSong = () => {
-        if(songIndex < songs2.length -1){
+        if(songIndex < songs.length -1){
             clickPlay();
             setSongIndex(songIndex + 1);
         }else
@@ -98,7 +134,7 @@ const GuessingGame = (props) => {
                 </div>
 
                 <div>
-                { songs2.length > 0 ? 
+                { songs.length > 0 ? 
                     <div>
                         <GameWelcome artist={artist} setGameStart={setGameStart} gameStart={gameStart}></GameWelcome>
 
@@ -110,7 +146,7 @@ const GuessingGame = (props) => {
                             <div className="flex min-h-screen justify-center">
                                 <div className="grid max-h-72 min-h-screen min-w-[100%] grid-rows-4 text-center gap-4">
                                     <div className="mt-10">
-                                        {songs2.map((s, i) => { 
+                                        {songs.map((s, i) => { 
                                             return(
                                                 <div className={ classNames.bind(styles)({
                                                     "MidiPlayer": true,
@@ -130,7 +166,7 @@ const GuessingGame = (props) => {
                                         })}
                                     </div>
                                     <div className="content-around">
-                                        <GuessSong songTitle={songs2[songIndex].title} nextSong={nextSong} toastCorrectGuess={toastCorrectGuess} toastIncorrectGuess={toastIncorrectGuess}/>
+                                        <GuessSong songTitle={songs[songIndex].title} nextSong={nextSong} toastCorrectGuess={toastCorrectGuess} toastIncorrectGuess={toastIncorrectGuess}/>
                                     </div>
                                     <div>
                                         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={skipSong}>Skip Song</button>
@@ -139,7 +175,7 @@ const GuessingGame = (props) => {
                             </div>
                         </div>
                         
-                        <GameComplete artist={artist} songsList={songs2} gameOver={gameOver}></GameComplete>
+                        <GameComplete artist={artist} songsList={songs} gameOver={gameOver}></GameComplete>
                     </div>
                 :
                         <h1>Loading data</h1>
