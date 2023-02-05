25650c30408bf28383eb534e3585f87cec043f63
Christopher Bohl <chrisrbohl@gmail.com>
2023-02-05 13:57:32 -0600

    Adds a nav bar.

--- src/components/GuessingGame.tsx
+++ src/components/GuessingGame.tsx
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import toast, { Toaster } from 'react-hot-toast';
+import NavBar from './NavBar';
 import GameWelcome from './GameWelcome';
 import GuessSong from './GuessSong';
 import GameComplete from './GameComplete';
@@ -122,6 +123,7 @@ const GuessingGame = () => {
 
   return (
     <div>
+      <NavBar></NavBar>
       <div>
         {songs.length > 0 ? (
           <div>
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useQuery, gql } from '@apollo/client';
+import NavBar from '../components/NavBar';
 
 const GET_ARTISTS = gql`
   query allArtists {
@@ -19,36 +20,39 @@ export default function Root() {
     return <h1>GraphQL query error!</h1>;
   }
   return (
-    <div className="flex min-h-screen justify-center">
-      <div className="grid max-w-3xl min-w-[60%] max-h-screen grid-rows-4 text-center">
-        <h1 className="rows-span-1">Welcome to TuneTest!</h1>
-        <nav>
-          <ul className="grid grid-rows-3 max-h-screen mx-auto">
-            {data.allArtists.map((artist, i) => {
-              return (
-                <li className="rows-span-1" key={i}>
-                  <div className="mx-auto block lg:h-5/6 lg:w-1/3 sm:h-5/6 sm:w-1/4 h-2/3 w-1/2 rounded-md overflow-hidden group">
-                    <div className="absolute my-auto justify-center items-center left-1/2 transform -translate-x-1/2 z-30 hidden group-hover:block">
-                      <Link
-                        to={'games/' + (i + 1)}
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        Play The {artist.lastName} Game
-                      </Link>
+    <div>
+      <NavBar></NavBar>
+      <div className="flex min-h-screen justify-center">
+        <div className="grid max-w-3xl min-w-[60%] max-h-screen grid-rows-4 text-center">
+          <h1 className="rows-span-1">Welcome to TuneTest!</h1>
+          <nav>
+            <ul className="grid grid-rows-3 max-h-screen mx-auto">
+              {data.allArtists.map((artist, i) => {
+                return (
+                  <li className="rows-span-1" key={i}>
+                    <div className="mx-auto block lg:h-5/6 lg:w-1/3 sm:h-5/6 sm:w-1/4 h-2/3 w-1/2 rounded-md overflow-hidden group">
+                      <div className="absolute my-auto justify-center items-center left-1/2 transform -translate-x-1/2 z-30 hidden group-hover:block">
+                        <Link
+                          to={'games/' + (i + 1)}
+                          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                          Play The {artist.lastName} Game
+                        </Link>
+                      </div>
+                      <img
+                        src={
+                          process.env.REACT_APP_API_URL +
+                          '/static/images/' +
+                          artist.lastName +
+                          '2.webp'
+                        }
+                        className="object-cover scale-100 hover:scale-125 ease-in duration-500 z-10"></img>
                     </div>
-                    <img
-                      src={
-                        process.env.REACT_APP_API_URL +
-                        '/static/images/' +
-                        artist.lastName +
-                        '2.webp'
-                      }
-                      className="object-cover scale-100 hover:scale-125 ease-in duration-500 z-10"></img>
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </div>
       </div>
     </div>
   );
