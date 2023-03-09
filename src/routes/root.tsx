a43e441ab530882b3bce280a6425089f7b01f69e
Christopher Bohl <chrisrbohl@gmail.com>
2023-03-09 12:45:29 -0600

    Makes major UI improvements.

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -32,7 +32,7 @@ export default function Root() {
     return <h1>GraphQL query error!</h1>;
   }
   return (
-    <div className="h-auto bg-[#8A6464]">
+    <div className="h-auto bg-rose">
       <NavBar></NavBar>
       <div id="sine-wave-window" className="row-span-2">
         <SineWave></SineWave>
@@ -41,20 +41,23 @@ export default function Root() {
       <div id="sine-wave-window-2" className="row-span-2">
         <SineWave></SineWave>
       </div> */}
-      <div className="flex justify-center bg-black w-4/5">
-        <div className="grid grid-rows-6 w-full max-h-screen">
-          <div className="row-span-1 grid place-items-center">
-            {/* <div>
+      <div className=" grid place-items-center">
+        <div className="flex justify-center bg-rose-dark w-4/5">
+          <div className="grid grid-rows-6 w-full max-h-screen">
+            <div className="row-span-1 grid place-items-center">
+              {/* <div>
               <h1>Welcome to TuneTest!</h1>
             </div> */}
-            {/* <div className="w-1/4  "> */}
-            <div>
-              <img src={'/tunetestlogo.png'} className="w-60"></img>
+              {/* <div className="w-1/4  "> */}
+              <div>
+                <a href={'/'}>
+                  <img src={'/tunetestlogo.png'} className="w-60"></img>
+                </a>
+              </div>
             </div>
-          </div>
-          {/* <nav> */}
+            {/* <nav> */}
 
-          {/*          
+            {/*          
           <div className="row-span-3 grid grid-rows-3 w-4/5 bg-white h-full">
             
         
@@ -72,32 +75,33 @@ export default function Root() {
 
           </div> */}
 
-          <nav className="row-span-5 grid grid-rows-3 h-full w-full">
-            {data.allArtists.map((artist: Artist, i: number) => {
-              return (
-                <div className="row-span-1" key={i}>
-                  <div className="mx-auto block xl:w-1/5 lg:h-5/6 lg:w-1/4 sm:h-5/6 sm:w-1/4 h-2/3 w-1/2 rounded-md overflow-hidden group">
-                    <div className="absolute my-auto justify-center items-center left-1/2 transform -translate-x-1/2 z-30 hidden group-hover:blockabsolute my-auto justify-center items-center left-1/2 transform -translate-x-1/2 z-30 hidden group-hover:block">
-                      <Link
-                        to={'games/' + (i + 1)}
-                        className="flex flex-col items-center text-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        Play The {artist.lastName} Game
-                      </Link>
+            <nav className="row-span-5 grid grid-rows-3 h-full w-full">
+              {data.allArtists.map((artist: Artist, i: number) => {
+                return (
+                  <div className="row-span-1" key={i}>
+                    <div className="mx-auto block xl:w-1/4 lg:h-5/6 lg:w-1/3 md:w-1/3 sm:h-5/6 sm:w-1/3 h-2/3 w-1/2 rounded-md overflow-hidden group">
+                      <div className="absolute my-auto justify-center items-center left-1/2 transform -translate-x-1/2 z-30 hidden group-hover:blockabsolute my-auto justify-center items-center left-1/2 transform -translate-x-1/2 z-30 hidden group-hover:block">
+                        <Link
+                          to={'games/' + (i + 1)}
+                          className="font-white flex flex-col items-center text-center bg-cloud hover:bg-cloud-dark font-bold text-slate-50 py-2 px-4 rounded">
+                          Play The {artist.lastName} Game
+                        </Link>
+                      </div>
+                      <img
+                        src={
+                          process.env.REACT_APP_API_URL +
+                          '/static/images/' +
+                          artist.lastName +
+                          '2.webp'
+                        }
+                        className="object-contain scale-100 hover:scale-125 ease-in duration-500"></img>
                     </div>
-                    <img
-                      src={
-                        process.env.REACT_APP_API_URL +
-                        '/static/images/' +
-                        artist.lastName +
-                        '2.webp'
-                      }
-                      className="object-contain scale-100 hover:scale-125 ease-in duration-500"></img>
                   </div>
-                </div>
-              );
-            })}
-          </nav>
-          {/* </nav> */}
+                );
+              })}
+            </nav>
+            {/* </nav> */}
+          </div>
         </div>
       </div>
       <div id="sine-wave-window-2" className="row-span-2">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      cloud: {
+        DEFAULT: '#6D7C95',
+        dark: '#3C5073',
+      },
+      rose: {
+        DEFAULT: '#8A6464',
+        dark: '#331A1A',
+      },
+    },
+    fontFamily: {
+      cool: ['Geneva'],
+    },
     extend: {},
   },
   plugins: [],
   variants: {
     extend: {
-      visibility: ["group-hover"],
+      visibility: ['group-hover'],
     },
-   },
-}
+  },
+};
